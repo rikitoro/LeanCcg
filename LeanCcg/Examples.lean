85bbb181a9d76773c699d4af057f82543fc21e68
rikitoro <>
2026-01-14 10:14:22 +0900

    ChartとLexiconにHashMapを導入

--- LeanCcg/Examples.lean
+++ LeanCcg/Examples.lean
@@ -2,39 +2,36 @@ import LeanCcg.Parsing
 
 /- # Examples -/
 
-def lexicon1 : Token → List Cat
-  | "Keats"   => [.NP]
-  | "eats"    => [.S \> .NP /> .NP]
-  | "cooks"   => [.S \> .NP /> .NP]
-  | "an"      => [.NP /> .N]
-  | "apple"   => [.N]
-  | "apples"  => [.NP]
-  | _         => []
+def lexicon1 : Lexicon := {
+  ("Keats",    [.NP]),
+  ("eats",     [.S \> .NP /> .NP]),
+  ("cooks",    [.S \> .NP /> .NP]),
+  ("an",       [.NP /> .N]),
+  ("apple",    [.N]),
+  ("apples",   [.NP])}
 
 #eval parse lexicon1 "Keats eats apples"
 #eval parse lexicon1 "Keats eats an apple"
 
-def lexicon2 : Token → List Cat
-  | "I"       => [.S /> (.S \> .NP)]
-  | "think"   => [.S \> .NP /> .S,]
-  | "that"    => [.S /> .S, .NP /> .N, .N, .N \> .N /> (.S /> .NP)]
-  | "boy"     => [.N]
-  | "wrote"   => [.S \> .NP /> .NP]
-  | "is"      => [.S \> .NP /> (.S \> .NP)]
-  | "wrong"   => [.S \> .NP]
-  | _ => []
+def lexicon2 : Lexicon := {
+  ("I",       [.S /> (.S \> .NP)]),
+  ("think",   [.S \> .NP /> .S,]),
+  ("that",    [.S /> .S, .NP /> .N, .N, .N \> .N /> (.S /> .NP)]),
+  ("boy",     [.N]),
+  ("wrote",   [.S \> .NP /> .NP]),
+  ("is",      [.S \> .NP /> (.S \> .NP)]),
+  ("wrong",   [.S \> .NP])}
 
 #eval parse lexicon2 "I think that that that that that boy wrote is wrong"
 
-def lexicon3 : Token → List Cat
-  | "Keats"   => [.NP]
-  | "will"    => [.S \> .NP /> (.S \> .NP)]
-  | "file"    => [.S \> .NP /> .NP]
-  | "without" => [.S \> .NP \> (.S \> .NP) /> (.S \> .NP)]
-  | "reading" => [.S \> .NP /> .NP]
-  | "these"   => [.NP /> .NP]
-  | "articles"  => [.NP]
-  | _ => []
+def lexicon3 : Lexicon := {
+  ("Keats",     [.NP]),
+  ("will",      [.S \> .NP /> (.S \> .NP)]),
+  ("file",      [.S \> .NP /> .NP]),
+  ("without",   [.S \> .NP \> (.S \> .NP) /> (.S \> .NP)]),
+  ("reading",   [.S \> .NP /> .NP]),
+  ("these",     [.NP /> .NP]),
+  ("articles",  [.NP])}
 
 
 #eval parse lexicon3 "file without reading"
--- LeanCcg/Parsing.lean
+++ LeanCcg/Parsing.lean
@@ -1,55 +1,51 @@
 import LeanCcg.Applying
+import Std
 
 /- # CYKアルゴリズムによるチャートパーシング -/
 
-/- ## 各セルの定義 -/
-structure Cell where
-  span  : Nat
-  index : Nat
-  trees : List Tree
+/-- 範疇辞書の型 -/
+abbrev Lexicon := Std.HashMap Token (List Cat)
 
-def Cell.toString : Cell → String
-  | ⟨span, index, trees⟩ => s!"⟨{span}, {index}, {trees}⟩"
+def Lexicon.getCats (lex : Lexicon) (t : Token) : List Cat :=
+  lex.getD t []
 
-instance : ToString Cell where
-  toString := Cell.toString
 
-abbrev Chart := List Cell
+/-- chart の型
+  key : (span, i), value : index = i ~ i+span の部分の導出木の集まり
+-/
+abbrev Chart := Std.HashMap (Nat × Nat) (List Tree) -- key : (span, i)
 
-def Chart.lookup (chart : Chart) (span index : Nat) : List Tree :=
-  let cell : Option Cell :=
-    chart.find? fun ⟨s', i', _⟩ ↦ s' = span && i' = index
-  match cell with
-  | some ⟨_, _, ts⟩ => ts
-  | none => []
+def Chart.getTrees (chart : Chart) (span_i : Nat×Nat) :=
+  chart.getD span_i []
 
 /-- ## CYK パーシング -/
-def fillChart (lexicon : Token → List Cat) (toks : List Token)  : Chart := Id.run do
+def fillChart (lex : Lexicon) (toks : List Token)  : Chart := Id.run do
   let len := toks.length
+  let mut chart : Chart := {}
   -- リーフノードを作成 (span = 1)
-  let mut chart : Chart :=
-    toks.zipIdx.map fun (t, i) ↦
-      let leaves : List Tree := (lexicon t).map (.leaf t ·)
-      let leaves' : List Tree := leaves.flatMap (raiseTree ·) -- Type raising したもの
-      ⟨1, i, leaves ++ leaves'⟩
+  let span : Nat := 1
+  for (t, i) in toks.zipIdx do
+    let leaves : List Tree := (lex.getCats t).map (.leaf t ·)
+    let raised : List Tree := leaves.flatMap (raiseTree ·)
+    chart := chart.insert (span, i) (leaves ++ raised)
   -- ボトムアップに導出木を結合 (span ≥ 2)
   for span in 2...(len + 1) do
     for i in 0...(len - span + 1) do
       let mut trees : List Tree := []
       for k in 1...span do
-        let lts := chart.lookup k i
-        let rts := chart.lookup (span - k) (i + k)
+        let lts := chart.getTrees (k, i)
+        let rts := chart.getTrees (span - k, i + k)
         trees := trees ++
           lts.flatMap fun lt ↦ rts.flatMap fun rt ↦ combineTree lt rt
-      let trees' : List Tree := trees.flatMap (raiseTree ·) -- Type raising したもの
-      chart := chart.concat ⟨span, i, trees ++ trees'⟩
+      let raised : List Tree := trees.flatMap (raiseTree ·) -- Type raising したもの
+      chart := chart.insert (span, i) (trees ++ raised)
   return chart
 
-def parseCCG  (lexicon : Token → List Cat) (toks : List Token) : List Tree :=
-  let chart := fillChart lexicon toks
+def parseCCG  (lex : Lexicon) (toks : List Token) : List Tree :=
+  let chart := fillChart lex toks
   let len := toks.length
-  chart.lookup len 0
+  chart.getTrees (len, 0)
 
-def parse  (lexicon : Token → List Cat) (sentence : String): List Tree :=
+def parse  (lex : Lexicon) (sentence : String): List Tree :=
   let toks : List Token := sentence.toTokens
-  parseCCG lexicon toks
+  parseCCG lex toks
